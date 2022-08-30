ae79321b87f28b17a610b469258b9db58108a937
samantha debenedictis <samdebenedictis@aol.com>
2022-08-30 14:37:13 -0400

    commit tree e1b373241c8620c10956e84ea091fabc6c88a3c0

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(x){
+ if(x<400)
+ return('This one is on me!')
+ else if(x>400 && x<2000)
+ return('That will be twenty bucks.')
+ else if(x>2000 && x<2500)
+ return('I will gladly take your thirty bucks.')
+ else if(x>2500)
+ return('No can do.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+return(city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string){
+  switch(string){
+    case 'generous':
+    return'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      case 'thanks for everything':
+        return 'Bye.'
+  }
 }
